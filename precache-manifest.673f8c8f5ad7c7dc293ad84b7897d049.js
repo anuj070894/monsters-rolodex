49a896fc9cdf4d7382d28d629cf916f055bb3295
Anuj Kumar <anujkuma@adobe.com>
2020-04-26 17:06:12 +0530

    Updates

---
+++ precache-manifest.673f8c8f5ad7c7dc293ad84b7897d049.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7207b6d36667dc263ed67509cc5893ae",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "469455c57529b4d29344",
+    "url": "/monsters-rolodex/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "fa891a2a4470c6c963fb",
+    "url": "/monsters-rolodex/static/js/2.17c60e7a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.17c60e7a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "469455c57529b4d29344",
+    "url": "/monsters-rolodex/static/js/main.3b37f072.chunk.js"
+  },
+  {
+    "revision": "46776c41254805b31b7c",
+    "url": "/monsters-rolodex/static/js/runtime-main.1dd86d7a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/monsters-rolodex/static/media/logo.5d5d9eef.svg"
+  }
+]);
